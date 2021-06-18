ec20100bc04893b433c887052b3967cafee194a4
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2021-06-18 21:30:32 +0200

    Update script.js

--- test-009-projectmanagment/script.js
+++ test-009-projectmanagment/script.js
@@ -1,3 +1,28 @@
+const toggleSwitch = document.querySelector('.theme-switch svg[id="dark-mode-switch"]');
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('class', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('class', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('class', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   var modeSwitch = document.querySelector('.mode-switch');
 
