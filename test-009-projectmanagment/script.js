a0bc3cc3d865fee0603aec998ce3ae32d478cc2e
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2021-06-18 21:34:13 +0200

    Update script.js

--- test-009-projectmanagment/script.js
+++ test-009-projectmanagment/script.js
@@ -1,43 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var modeSwitch = document.querySelector('.mode-switch');
-
-  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
-    modeSwitch.classList.toggle('active');
-  });
-  
-  var listView = document.querySelector('.list-view');
-  var gridView = document.querySelector('.grid-view');
-  var projectsList = document.querySelector('.project-boxes');
-  
-  listView.addEventListener('click', function () {
-    gridView.classList.remove('active');
-    listView.classList.add('active');
-    projectsList.classList.remove('jsGridView');
-    projectsList.classList.add('jsListView');
-  });
-  
-  gridView.addEventListener('click', function () {
-    gridView.classList.add('active');
-    listView.classList.remove('active');
-    projectsList.classList.remove('jsListView');
-    projectsList.classList.add('jsGridView');
-  });
-  
-  document.querySelector('.messages-btn').addEventListener('click', function () {
-    document.querySelector('.messages-section').classList.add('show');
-  });
-  
-  document.querySelector('.messages-close').addEventListener('click', function() {
-    document.querySelector('.messages-section').classList.remove('show');
-  });
-});
-
-
-
-const toggleSwitch = document.querySelector('.theme-switch svg[id="dark-mode-switch"]');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
-    document.documentElement.setAttribute('class', currentTheme);
+    document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
@@ -45,10 +9,11 @@ if (currentTheme) {
 }
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('class', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
-    else {        document.documentElement.setAttribute('class', 'light');
+    else {        document.documentElement.setAttribute('data-theme', 'light');
           localStorage.setItem('theme', 'light');
     }    
 }
+toggleSwitch.addEventListener('change', switchTheme, false);
